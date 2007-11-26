cc5d4fb70c13d98139fb1cee9b9100b74f040989
Alistair Young <alistair@codebrane,com>
2007-11-26 15:18:56 +0000

    Now uses empty DigitalItemInfo for root collection

--- content-chh/content-chh-impl/impl/src/java/uk/ac/uhi/ral/impl/util/TypeResolver.java
+++ content-chh/content-chh-impl/impl/src/java/uk/ac/uhi/ral/impl/util/TypeResolver.java
@@ -8,7 +8,6 @@ package uk.ac.uhi.ral.impl.util;
 import org.sakaiproject.content.api.ContentEntity;
 import org.sakaiproject.content.api.ContentHostingHandler;
 import org.sakaiproject.content.api.ContentHostingHandlerResolver;
-import org.sakaiproject.content.chh.fedora.ContentCollectionFedora;
 import uk.ac.uhi.ral.DigitalItemInfo;
 import uk.ac.uhi.ral.DigitalRepository;
 
@@ -24,24 +23,32 @@ public class TypeResolver {
   public static ContentEntity resolveEntity(ContentEntity realParent, String relativePath,
                                             ContentHostingHandler chh,
                                             ContentHostingHandlerResolver chhResolver,
-                                            DigitalRepository repo,
-                                            DigitalItemInfo item) {
+                                            DigitalRepository repo) {
     while (relativePath.length() > 0 && relativePath.charAt(0) == '/')
       relativePath = relativePath.substring(1);
     while (relativePath.length() > 0
         && relativePath.charAt(relativePath.length() - 1) == '/')
       relativePath = relativePath.substring(0, relativePath.length() - 1);
 
+    DigitalItemInfo item = null;
+
     // Root of the mount, so it's a collection
     if (relativePath.equals("")) {
-      relativePath = "/";
-      ContentCollectionFedora collection = new ContentCollectionFedora(realParent, relativePath, chh, chhResolver, repo, item);
-      collection.wrap();
-      return collection;
+      item = repo.generateItem();
+      relativePath = "/" + relativePath;
+      item.setIsCollection(true);
+      item.setIsResource(false);
+      item.setDescription("TEST");
+      item.setCreator("TEST");
+      item.setDisplayName("TEST");
+      item.setModifiedDate("TEST");
+      item.setDisplayName("TEST");
+    }
+    else {
+      item = repo.list(relativePath);
+      item.setIsResource(true);
     }
 
-    // relativePath will be the pid to get the data for
-    relativePath = "/" + relativePath;
     return TypeMapper.toContentEntity(item, realParent, relativePath, chh, chhResolver, repo);
   }
 }
