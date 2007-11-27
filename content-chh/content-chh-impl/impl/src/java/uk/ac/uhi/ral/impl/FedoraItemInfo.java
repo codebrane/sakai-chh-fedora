8e9bdf1c170b2c19b6e8cf25f5d4d31f97d82de1
Alistair Young <alistair@codebrane,com>
2007-11-27 15:49:58 +0000

    Updated to latest interface

--- content-chh/content-chh-impl/impl/src/java/uk/ac/uhi/ral/impl/FedoraItemInfo.java
+++ content-chh/content-chh-impl/impl/src/java/uk/ac/uhi/ral/impl/FedoraItemInfo.java
@@ -8,16 +8,78 @@ package uk.ac.uhi.ral.impl;
 import uk.ac.uhi.ral.DigitalItemInfo;
 
 public class FedoraItemInfo implements DigitalItemInfo {
+  private String title = null;
+  private String subject = null;
+  private String description = null;
+  private String publisher = null;
+  private String identifier = null;
+  private String mimeType = null;
+  private byte[] binaryContent = null;
   private String displayName = null;
   private String creator = null;
   private String modifiedDate = null;
   private String originalFilename = null;
-  private String description = null;
   private String type = null;
-  private long contentLength;
   private boolean resource;
   private boolean collection;
   private Object privateInfo;
+  private String url = null;
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setSubject(String subject) {
+    this.subject = subject;
+  }
+
+  public String getSubject() {
+    return subject;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setPublisher(String publisher) {
+    this.publisher = publisher;
+  }
+
+  public String getPublisher() {
+    return publisher;
+  }
+
+  public void setIdentifier(String identifier) {
+    this.identifier = identifier;
+  }
+
+  public String getIdentifier() {
+    return identifier;
+  }
+
+  public void setMimeType(String mimeType) {
+    this.mimeType = mimeType;
+  }
+
+  public String getMimeType() {
+    return mimeType;
+  }
+
+  public void setBinaryContent(byte[] binaryContent) {
+    this.binaryContent = binaryContent;
+  }
+
+  public byte[] getBinaryContent() {
+    return binaryContent;
+  }
 
   public void setDisplayName(String displayName) {
     this.displayName = displayName;
@@ -51,14 +113,6 @@ public class FedoraItemInfo implements DigitalItemInfo {
     return originalFilename;
   }
 
-  public void setDescription(String description) {
-    this.description = description;
-  }
-
-  public String getDescription() {
-    return description;
-  }
-
   public void setType(String type) {
     this.type = type;
   }
@@ -67,12 +121,8 @@ public class FedoraItemInfo implements DigitalItemInfo {
     return type;
   }
 
-  public void setContentLength(long contentLength) {
-    this.contentLength = contentLength;
-  }
-
-  public long getContentLength() {
-    return contentLength;
+  public int getContentLength() {
+    return binaryContent.length;
   }
 
   public void setIsResource(boolean resource) {
@@ -97,5 +147,13 @@ public class FedoraItemInfo implements DigitalItemInfo {
 
   public Object getPrivateInfo() {
     return privateInfo;
+  }
+
+  public void setURL(String url) {
+    this.url = url;
+  }
+
+  public String getURL() {
+    return url;
   }
 }
