1217162fb6763341296251c6862028c084848a35
Alistair Young <alistair@codebrane,com>
2007-11-28 13:29:27 +0000

    Added dumpXML()

--- content-chh/content-chh-impl/impl/src/java/uk/ac/uhi/ral/impl/util/Utils.java
+++ content-chh/content-chh-impl/impl/src/java/uk/ac/uhi/ral/impl/util/Utils.java
@@ -8,10 +8,36 @@ package uk.ac.uhi.ral.impl.util;
 import org.apache.xmlbeans.XmlObject;
 import org.apache.xmlbeans.XmlOptions;
 
-import java.util.HashMap;
+import java.io.File;
 import java.io.StringWriter;
+import java.util.HashMap;
 
 public class Utils {
+  /**
+   * Dumps an XML document to disk.
+   *
+   * @param doc The document to dump to disk
+   * @param filePath The full path and name of the file
+   */
+  public static void dumpXML(XmlObject doc, String filePath) {
+    HashMap<String, String> namespaces = new HashMap<String, String>();
+    namespaces.put("http://www.w3.org/1999/02/22-rdf-syntax-ns#", "rdf");
+    namespaces.put("http://www.nsdl.org/ontologies/relationships#", "myns");
+    XmlOptions xmlOptions = new XmlOptions();
+    xmlOptions.setSavePrettyPrint();
+    xmlOptions.setSavePrettyPrintIndent(2);
+    //xmlOptions.setUseDefaultNamespace();
+    //xmlOptions.setSaveAggressiveNamespaces();
+    xmlOptions.setSaveSuggestedPrefixes(namespaces);
+    //xmlOptions.setSaveNamespacesFirst();
+
+    try {
+      doc.save(new File(filePath), xmlOptions);
+    }
+    catch(Exception e) {
+    }
+  }
+
   /**
    * Returns an XMLBeans document as a String with full Fedora/RDF namespace support
    *
