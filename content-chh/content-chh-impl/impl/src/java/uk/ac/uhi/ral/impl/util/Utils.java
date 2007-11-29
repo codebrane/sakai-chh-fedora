2f5e69995c505679b8d3bcf138212537d1266bc4
Alistair Young <alistair@codebrane,com>
2007-11-29 11:17:04 +0000

    Added getDCBytes()

--- content-chh/content-chh-impl/impl/src/java/uk/ac/uhi/ral/impl/util/Utils.java
+++ content-chh/content-chh-impl/impl/src/java/uk/ac/uhi/ral/impl/util/Utils.java
@@ -7,10 +7,18 @@ package uk.ac.uhi.ral.impl.util;
 
 import org.apache.xmlbeans.XmlObject;
 import org.apache.xmlbeans.XmlOptions;
+import org.w3c.dom.Element;
+import org.w3c.dom.Text;
+import uk.ac.uhi.ral.DigitalItemInfo;
 
-import java.io.File;
-import java.io.StringWriter;
+import java.io.*;
 import java.util.HashMap;
+
+import fedora.fedoraSystemDef.foxml.DatastreamType;
+import fedora.fedoraSystemDef.foxml.StateType;
+import fedora.fedoraSystemDef.foxml.DatastreamVersionType;
+import fedora.fedoraSystemDef.foxml.XmlContentType;
+import info.fedora.definitions.x1.x0.types.Datastream;
 
 public class Utils {
   /**
@@ -59,5 +67,75 @@ public class Utils {
     catch(Exception e) {
       return null;
     }
+  }
+
+  public static byte[] getContentBytes(InputStream is) {
+    int bytesRead = 0;
+    byte[] buffer = new byte[1024];
+    ByteArrayOutputStream bytes = new ByteArrayOutputStream(2048);
+    try {
+      while ((bytesRead = is.read(buffer)) != -1) {
+        bytes.write(buffer, 0, bytesRead);
+      }
+
+      return bytes.toByteArray();
+    }
+    catch (IOException e) {
+      return null;
+    }
+  }
+
+  public static byte[] getDCBytes(DigitalItemInfo item, Datastream ds) {
+    DatastreamType dcDatastream = DatastreamType.Factory.newInstance();
+    dcDatastream.setID("DC");
+    dcDatastream.setSTATE(StateType.A);
+    dcDatastream.setCONTROLGROUP(DatastreamType.CONTROLGROUP.X);
+
+    DatastreamVersionType dcDatastreamVersion = dcDatastream.addNewDatastreamVersion();
+    dcDatastreamVersion.setID(ds.getID());
+    dcDatastreamVersion.setMIMETYPE(ds.getMIMEType());
+    dcDatastreamVersion.setLABEL(ds.getLabel());
+
+    XmlContentType xmlContent = dcDatastreamVersion.addNewXmlContent();
+
+    Element dc = xmlContent.getDomNode().getOwnerDocument().createElementNS("oai_dc", "dc");
+
+    // http://www.fedora.info/bugzilla/show_bug.cgi?id=153
+    dc.setAttribute("xmlns:oai_dc", "http://www.openarchives.org/OAI/2.0/oai_dc/");
+    dc.setAttribute("xmlns:dc", "http://purl.org/dc/elements/1.1/");
+
+    Element dcTitle = xmlContent.getDomNode().getOwnerDocument().createElementNS("dc", "title");
+    Text textNode = xmlContent.getDomNode().getOwnerDocument().createTextNode(item.getTitle());
+    dcTitle.appendChild(textNode);
+    dc.appendChild(dcTitle);
+
+    Element dcCreator = xmlContent.getDomNode().getOwnerDocument().createElementNS("dc", "creator");
+    textNode = xmlContent.getDomNode().getOwnerDocument().createTextNode(item.getCreator());
+    dcCreator.appendChild(textNode);
+    dc.appendChild(dcCreator);
+
+    Element dcSubject = xmlContent.getDomNode().getOwnerDocument().createElementNS("dc", "subject");
+    textNode = xmlContent.getDomNode().getOwnerDocument().createTextNode(item.getSubject());
+    dcSubject.appendChild(textNode);
+    dc.appendChild(dcSubject);
+
+    Element dcDescription = xmlContent.getDomNode().getOwnerDocument().createElementNS("dc", "description");
+    textNode = xmlContent.getDomNode().getOwnerDocument().createTextNode(item.getDescription());
+    dcDescription.appendChild(textNode);
+    dc.appendChild(dcDescription);
+
+    Element dcPublisher = xmlContent.getDomNode().getOwnerDocument().createElementNS("dc", "publisher");
+    textNode = xmlContent.getDomNode().getOwnerDocument().createTextNode(item.getPublisher());
+    dcPublisher.appendChild(textNode);
+    dc.appendChild(dcPublisher);
+
+    Element dcIdentifier = xmlContent.getDomNode().getOwnerDocument().createElementNS("dc", "identifier");
+    textNode = xmlContent.getDomNode().getOwnerDocument().createTextNode(item.getTitle());
+    dcIdentifier.appendChild(textNode);
+    dc.appendChild(dcIdentifier);
+
+    xmlContent.getDomNode().appendChild(dc);
+
+    return xmlContent.toString().getBytes();
   }
 }
