27025781741d2aa45e06c97b2bf8c6f6d8afee3a
Alistair Young <alistair@codebrane,com>
2007-11-29 11:11:38 +0000

    Added fields for DC and RELS-EXT identifiers for updating the metadata

--- content-chh/content-chh-impl/impl/src/java/uk/ac/uhi/ral/impl/FedoraPrivateItemInfo.java
+++ content-chh/content-chh-impl/impl/src/java/uk/ac/uhi/ral/impl/FedoraPrivateItemInfo.java
@@ -8,6 +8,8 @@ package uk.ac.uhi.ral.impl;
 public class FedoraPrivateItemInfo {
   private String pid = null;
   private String ownerId = null;
+  private String dcDatastreamID = null;
+  private String relsextDatastreamID = null;
 
   public String getOwnerId() {
     return ownerId;
@@ -23,5 +25,21 @@ public class FedoraPrivateItemInfo {
 
   public void setPid(String pid) {
     this.pid = pid;
+  }
+
+  public void setDCDatastreamID(String dcDatastreamID) {
+    this.dcDatastreamID = dcDatastreamID;
+  }
+
+  public String getDCDatastreamID() {
+    return dcDatastreamID;
+  }
+
+  public void setRelsExtDatastreamID(String relsextDatastreamID) {
+    this.relsextDatastreamID = relsextDatastreamID;
+  }
+
+  public String getRelsExtDatastreamID() {
+    return relsextDatastreamID;
   }
 }
