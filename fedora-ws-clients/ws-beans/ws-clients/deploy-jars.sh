524990b62a2fb5c7cc1e8d245408b84c528b72fa
Alistair Young <alistair@codebrane,com>
2008-06-05 16:12:16 +0000

    Updated to use locally built jar instead of from repo

--- fedora-ws-clients/ws-beans/ws-clients/deploy-jars.sh
+++ fedora-ws-clients/ws-beans/ws-clients/deploy-jars.sh
@@ -1,18 +1,18 @@
 mvn deploy:deploy-file \
-    -Dfile=/Users/alistair/.m2/repository/fedora/fedora-api-a-client/2.2.1/fedora-api-a-client-2.2.1.jar \
+    -Dfile=build/api-a/client/3.0-b1/dist/fedora-api-a-client-3.0-b1.jar \
     -DgroupId=fedora \
     -DartifactId=fedora-api-a-client \
-    -Dversion=2.2.1 \
+    -Dversion=3.0-b1 \
     -Dpackaging=jar \
     -DgeneratePom=true \
     -DrepositoryId=source.uhi.ac.uk-repository \
     -Durl=dav:http://source.uhi.ac.uk/mvn
 
 mvn deploy:deploy-file \
-    -Dfile=/Users/alistair/.m2/repository/fedora/fedora-api-m-client/2.2.1/fedora-api-m-client-2.2.1.jar \
+    -Dfile=build/api-m/client/3.0-b1/dist/fedora-api-m-client-3.0-b1.jar \
     -DgroupId=fedora \
     -DartifactId=fedora-api-m-client \
-    -Dversion=2.2.1 \
+    -Dversion=3.0-b1 \
     -Dpackaging=jar \
     -DgeneratePom=true \
     -DrepositoryId=source.uhi.ac.uk-repository \
